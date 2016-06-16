94867918f6ad349b2f96cef66835677d7f09ca00
Nik van der Ploeg <nik.vanderploeg@jiff.com>
2016-06-15 17:40:06 -0700

    minor twaek

--- nik-theme.plugin.zsh
+++ nik-theme.plugin.zsh
@@ -17,10 +17,9 @@ ret_status="%(?:%{$PROMPT_SUCCESS_COLOR%}$prompt_thingy:%{$PROMPT_FAILURE_COLOR%
 reset_color=$FX[reset]
 # RPS1='%{$fg[blue]%}%~%{$reset_color%} '
 # RPS1='%{$fg[white]%}%2~$(git_prompt_info) %{$fg_bold[blue]%}%m%{$reset_color%}'
-if git_prompt_status > /dev/null 2>&1 ; then
-PROMPT='%{$nice_blue%}$prompt_hostname:%{$fg[green]%}%~%{$reset_color%} %{$GIT_PROMPT_INFO%}$(git_prompt_info)%{$GIT_DIRTY_COLOR%}$(git_prompt_status) %{$reset_color%}%{$PROMPT_PROMPT%}$ret_status%{$reset_color%}%(#~ #~) '
-else
 PROMPT='%{$nice_blue%}$prompt_hostname:%{$fg[green]%}%~%{$reset_color%} %{$reset_color%}%{$PROMPT_PROMPT%}$ret_status%{$reset_color%}%(#~ # ~) '
+if git_prompt_status > /dev/null 2>&1 ; then
+    PROMPT='%{$nice_blue%}$prompt_hostname:%{$fg[green]%}%~%{$reset_color%} %{$GIT_PROMPT_INFO%}$(git_prompt_info)%{$GIT_DIRTY_COLOR%}$(git_prompt_status) %{$reset_color%}%{$PROMPT_PROMPT%}$ret_status%{$reset_color%}%(#~ #~) '
 fi
 
 ZSH_THEME_GIT_PROMPT_PREFIX=" %{$fg[yellow]%}("
